4a50d8f9b6d02faea4b71aacb87a85f126e99aab
GustavoAntHer <117421154+GustavoAntHer@users.noreply.github.com>
2023-04-07 22:56:07 -0400

    Add files via upload

--- js/Main.js
+++ js/Main.js
@@ -1,9 +1,11 @@
 function menu(check,obj) {
 	var ver = check.checked;
-	if (ver == false) {
-		obj.style.display = 'block';
+	if (ver == true) {
+		obj.style.right = '0';
+		obj.style.width = '100vw';
 	}
 	else {
-		obj.style.display = 'none';
+		obj.style.width = '0';
+		obj.style.right = '-100vw';
 	}
 }
