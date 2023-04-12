922ac809f2fee55057ab57ce51a75bbac64821b2
GustavoAntHer <117421154+GustavoAntHer@users.noreply.github.com>
2023-04-12 11:30:14 -0400

    Add files via upload

--- js/Main.js
+++ js/Main.js
@@ -7,12 +7,30 @@ function menu(check,obj) {
 		obj.style.display = 'none';
 	}
 }
-function rotar(check,men) {
+function rotar(check,men, gal,ini,l) {
 	var ver = check.checked;
 	if (ver == true) {
 		men.style.rotate = '180deg';
+		gal.style.visibility = 'hidden';
+		ini.style.background = '#4163b2';
+		l.src= "2.2.1.png";
 	}
 	else {
 		men.style.rotate = '0deg';
+		gal.style.visibility = 'initial';
+		ini.style.background = "radial-gradient(rgba(0, 0, 0, .1),rgba(0, 0, 0, .2) ,rgba(0, 0, 0, .5))";
+		l.src= "2.2.png";
+	}
+}
+
+function rotar2(check,men, ini) {
+	var ver = check.checked;
+	if (ver == true) {
+		men.style.rotate = '180deg';
+		ini.style.background = '#4163b2';
+	}
+	else {
+		men.style.rotate = '0deg';
+		ini.style.background = 'rgba(222, 22, 75, 0.9)';
 	}
 }
